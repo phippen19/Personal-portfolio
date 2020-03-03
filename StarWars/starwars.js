f1256af3fef4d5b07e2e464387998cc08205f5bd
Ryan Phippen <phippen75@gmail.com>
2020-03-03 11:13:09 -0700

    Added Buttons and Removed Cast List

--- StarWars/starwars.js
+++ StarWars/starwars.js
@@ -1,37 +1,46 @@
 import { films } from '../Data/films.js'
 import { people } from '../Data/people.js'
 
-console.log("HI There! My first JavaScript code!")
-
-console.log(document.querySelector('.greeting'))
-
 let greetingDiv = document.querySelector('.greeting')
 
-let castList = document.createElement("ul")
+const maleCharacters = people.filter(person => person.gender === "male")
+console.log(maleCharacters)
+
+const femaleCharacters = people.filter(person => person.gender === "female")
+console.log(femaleCharacters)
+
+const otherCharacters = people.filter(person => person.gender === "n/a" || person.gender === "none" || person.gender === "hermaphrodite")
+console.log(otherCharacters)
+
+let maleButton = document.querySelector("#maleButton")
+let femaleButton = document.querySelector("#femaleButton")
+let otherButton = document.querySelector("#otherButton")
+
+maleButton.addEventListener("click", function (event) {
+    populateDOM(maleCharacters)
+})
 
 let counter = 1
 
-people.forEach((person) => {
-    let nameItem = document.createElement("li")
-    nameItem.textContent = person.name
-    castList.appendChild(nameItem)
+function populateDOM(characters) {
+    characters.forEach((person) => {
+        console.log(`${person.url}`)
+        let personAnchor = document.createElement("a")
+        personAnchor.href = "#"
+        let personImg = document.createElement("img")
+        personImg.src = `https://starwars-visualguide.com/assets/img/characters/${counter}.jpg`
 
-    let personAnchor = document.createElement("a")
-    personAnchor.href = "#"
-    let personImg = document.createElement("img")
-    personImg.src = `https://starwars-visualguide.com/assets/img/characters/${counter}.jpg`
+        personImg.addEventListener('error', (event) => {
+            personImg.hidden = true
+            //personImg.src = '../images/uvu.jpg'
+        })
 
-    personImg.addEventListener('error', (event) => {
-        personImg.hidden = true
-        //personImg.src = '../images/uvu.jpg'
-    });
+        personImg.addEventListener("click", function (event) {
+            console.log('Thanks for clicking')
+        })
 
-    personImg.addEventListener("click", function( event ){
-        console.log('Thanks for clicking')
+        personAnchor.appendChild(personImg)
+        greetingDiv.appendChild(personAnchor)
+        counter++
     })
-    personAnchor.appendChild(personImg)
-    greetingDiv.appendChild(personAnchor)
-    counter ++
-})
-
-greetingDiv.appendChild(castList)
+}
