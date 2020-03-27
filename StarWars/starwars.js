a1c6d2dd39423e76a07983280e35e023b0f0e78b
Ryan Phippen <phippen75@gmail.com>
2020-03-27 17:18:22 -0600

    Fixed characters page

--- StarWars/starwars.js
+++ StarWars/starwars.js
@@ -26,21 +26,6 @@ otherButton.addEventListener("click", function (event) {
     populateDOM(otherCharacters)
 })
 
-function getLastNumber(url) {
-    let end = url.lastIndexOf('/')
-    let start = end - 2
-    if(url.charAt(start) === '/') {
-        start++
-    }
-    return url.slice(start, end)
-}
-
-function removeChildren(element){
-    while (element.firstChild) {
-        element.removeChild(element.firstChild)
-    }
-}
-
 function populateDOM(characters) {
     removeChildren(gallery)
     characters.forEach((person) => {
