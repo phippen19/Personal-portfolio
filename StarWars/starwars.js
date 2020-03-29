5f2fefc6daa1c49e691ebd938c39d56f1d8f6560
Ryan Phippen <phippen75@gmail.com>
2020-03-28 22:50:03 -0600

    Added all characters button

--- StarWars/starwars.js
+++ StarWars/starwars.js
@@ -10,9 +10,12 @@ const femaleCharacters = people.filter(person => person.gender === "female")
 
 const otherCharacters = people.filter(person => person.gender === "n/a" || person.gender === "none" || person.gender === "hermaphrodite")
 
+const allCharacters = people.filter(person => person.gender === "male" || person.gender === "female" || person.gender === "n/a" || person.gender === "none" || person.gender === "hermaphrodite")
+
 let maleButton = document.querySelector("#maleButton")
 let femaleButton = document.querySelector("#femaleButton")
 let otherButton = document.querySelector("#otherButton")
+let allButton = document.querySelector("#allButton")
 
 maleButton.addEventListener("click", function (event) {
     populateDOM(maleCharacters)
@@ -26,12 +29,16 @@ otherButton.addEventListener("click", function (event) {
     populateDOM(otherCharacters)
 })
 
+allButton.addEventListener("click", function (event) {
+    populateDOM(allCharacters)
+})
+
 function populateDOM(characters) {
     removeChildren(gallery)
     characters.forEach((person) => {
         let imageNum = getLastNumber(person.url)
         let personAnchor = document.createElement("a")
-        personAnchor.href = "#"
+        //personAnchor.href = "#"
         let personImg = document.createElement("img")
         personImg.src = `https://starwars-visualguide.com/assets/img/characters/${imageNum}.jpg`
 
@@ -40,9 +47,9 @@ function populateDOM(characters) {
             //personImg.src = '../images/uvu.jpg'
         })
 
-        personImg.addEventListener("click", function (event) {
-            console.log('Thanks for clicking')
-        })
+        //personImg.addEventListener("click", function (event) {
+            //console.log('Thanks for clicking')
+        //})
 
         personAnchor.appendChild(personImg)
         gallery.appendChild(personAnchor)
