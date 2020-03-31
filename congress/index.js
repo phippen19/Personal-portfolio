44c3e79a6206265134b2afd021eea53a98ffa86e
Ryan Phippen <phippen75@gmail.com>
2020-03-31 16:28:05 -0600

    Added mapped senators and pics

--- congress/index.js
+++ congress/index.js
@@ -8,6 +8,30 @@ const filterSenators = (prop, value) => {
     return senators.filter(senator => senator[prop] === value)
 }
 
-console.log(filterSenators('party', 'R'))
-console.log(filterSenators('party', 'D'))
-console.log(filterSenators('party', 'ID'))
+const mappedSenators = senators.map(senator => {
+    let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
+    return {
+        id: senator.id,
+        name: `${senator.first_name}${middleName}${senator.last_name}`,
+        imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`
+    }
+})
+
+function populateContainer(smallSenatorsArray) {
+    console.log(smallSenatorsArray)
+    smallSenatorsArray.forEach(senator => {
+
+        let senFigure = document.createElement('figure')
+        let figImg = document.createElement('img')
+        let figCaption = document.createElement('figcaption')
+
+        figImg.src = senator.imgURL
+        figCaption.textContent = senator.name
+
+        senFigure.appendChild(figImg)
+        senFigure.appendChild(figCaption)
+        container.appendChild(senFigure)
+    })
+}
+
+populateContainer(mappedSenators)
