88a2fdb796335e639d986e9b62a2ccf26622f024
Ryan Phippen <phippen75@gmail.com>
2020-04-17 17:02:51 -0600

    Added images to cards!

--- pokemon/index.js
+++ pokemon/index.js
@@ -1,47 +1,46 @@
 let pokeContainer = document.querySelector('.pokeContainer')
+let startButton = document.querySelector('#startButton')
+startButton.addEventListener('click', () => {
+  loadPage()
+})
 
-/* function getPokeData(url) {
-  fetch(url).then(function (response) {
-    response.json().then(function (pokemon) {
-      console.log(pokemon.results)
-      populatePokeCards(pokemon.results)
-    })
-  })
-}
-
-getPokeData('https://pokeapi.co/api/v2/pokemon') */
-// https://pokeapi.co/api/v2/pokemon/1/
+let newButton = document.querySelector('#newButton')
+newButton.addEventListener('click', () => {
+  addPokemon()
+})
 
 async function getAPIData(url) {
-    try {
-        const response = await fetch(url)
-        const data = await response.json()
-        return data
-    }
-    catch (error) {
-        console.error(error)
-    }
+  try {
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+  }
+  catch (error) {
+    console.error(error)
+  }
 }
 
-getAPIData('https://pokeapi.co/api/v2/pokemon/?&limit=25').then(
+function loadPage() {
+  getAPIData('https://pokeapi.co/api/v2/pokemon/?&limit=25').then(
     (data) => {
-        for (const pokemon of data.results) {
-            getAPIData(pokemon.url).then(
-                (pokeData) => {
-                    populatePokeCard(pokeData)
-                }
-            )
-        }
+      for (const pokemon of data.results) {
+        getAPIData(pokemon.url).then(
+          (pokeData) => {
+            populatePokeCard(pokeData)
+          }
+        )
+      }
     }
-)
+  )
+}
 
 function populatePokeCard(singlePokemon) {
     let pokeScene = document.createElement('div')
     pokeScene.className = 'scene'
     let pokeCard = document.createElement('div')
     pokeCard.className = 'card'
-    pokeCard.addEventListener('click', function () {
-        pokeCard.classList.toggle('is-flipped')
+    pokeCard.addEventListener( 'click', function() {
+      pokeCard.classList.toggle('is-flipped')
     })
     let pokeFront = populateCardFront(singlePokemon)
     let pokeBack = populateCardBack(singlePokemon)
@@ -52,34 +51,49 @@ function populatePokeCard(singlePokemon) {
     pokeContainer.appendChild(pokeScene)
 }
 
-function populateCardFront(pokeMon) {
-    let cardFront = document.createElement('div')
-    cardFront.className = 'card__face card__face--front'
-    cardFront.textContent = `${pokeMon.name} ${pokeMon.id}`
-    let frontImage = document.createElement('img')
-    frontImage.src = '../images/uvu.jpg'
-    cardFront.appendChild(frontImage)
-    return cardFront
+function populateCardFront(pokemon) {
+  let cardFront = document.createElement('div')
+  cardFront.className = 'card__face card__face--front'
+
+  let frontImage = document.createElement('img')
+  frontImage.src = `../images/${getImageFileName(pokemon)}.png`
+  let frontLabel = document.createElement('p') 
+  frontLabel.textContent = `${pokemon.name.charAt(0).toUpperCase()}${pokemon.name.slice(1)}`
+  cardFront.appendChild(frontImage)
+  cardFront.appendChild(frontLabel)
+  return cardFront
 }
 
-function populateCardBack(pokeMon) {
-    let cardBack = document.createElement('div')
-    cardBack.className = 'card__face card__face--back'
-    cardBack.textContent = pokeMon.stats[0].stat.name
-    return cardBack
+function getImageFileName(pokemon) {
+  if (pokemon.id < 10) {
+    return `00${pokemon.id}`
+  } else if (pokemon.id > 9 && pokemon.id < 100) {
+    return `0${pokemon.id}`
+  } else return `pokeball`
+}
+
+function populateCardBack(pokemon) {
+  let cardBack = document.createElement('div')
+  cardBack.className = 'card__face card__face--back'
+  cardBack.textContent = pokemon.stats[0].stat.name
+  return cardBack
+}
+
+class Pokemon {
+  constructor(height, weight, name, stats) {
+    this.height = height
+    this.weight = weight
+    this.name = name
+    this.stats = stats
+    this.id = 900
   }
-  
-  {
-    /* <div class="scene">
-    <div class="card">
-      <div class="card__face card__face--front">front</div>
-      <div class="card__face card__face--back"><div><p>Hi,I'm here on the back</p></div></div>
-    </div>
-  </div>
-  
-  var card = document.querySelector('.card');
-  card.addEventListener( 'click', function() {
-    card.classList.toggle('is-flipped');
-  });
-  */
-  }
+}
+
+function addPokemon() {
+  let newPokemon = new Pokemon(50, 25, 'Thoremon', [
+    {
+      stat:
+        { name: 'Thunder Belly' }
+    }])
+  populatePokeCard(newPokemon)
+}
